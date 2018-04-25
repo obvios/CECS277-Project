396f12ca04181f46e36ee73d27194d50efd394d9
Eric Palma <palmae95@gmail.com>
2018-04-24 23:29:40 -0700

    Implemented Singleton pattern

--- RoachMotel.java
+++ RoachMotel.java
@@ -1,6 +1,6 @@
 /*
  * Class Name: RoachMotel
- * Purpose: To implement the singleton design patter
+ * Purpose: To implement the singleton design pattern
  */
 package designpatternsproject;
 
@@ -9,5 +9,15 @@ package designpatternsproject;
  * @author ericpalma
  */
 public class RoachMotel {
+    private static RoachMotel uniqueInstance;
     
+    //private constructor
+    private RoachMotel(){};
+    
+    public synchronized static RoachMotel getInstance(){
+        if(uniqueInstance == null){
+            uniqueInstance = new RoachMotel();
+        }
+        return uniqueInstance;
+    }
 }
