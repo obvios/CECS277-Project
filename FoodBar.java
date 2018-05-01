a31c736628b4e2084ea3d30483b678189fa35f51
JCPasillas <carlospasillas562@gmail.com>
2018-05-01 15:03:50 -0700

    some more

--- FoodBar.java
+++ FoodBar.java
@@ -1,9 +1,11 @@
 
-public class FoodBar {
+public class FoodBar extends Amnemities{
 	public static final COST = 10.00;
+	
 	public Spa() {
 		description = "Delicious Food Bar";
 	}
+	
 	public double cost() {
 		return COST;
 	}
--- Spa.java
+++ Spa.java
@@ -1,9 +1,11 @@
 
 public class Spa extends Amnemities{
 	public static final COST = 20.00;
+	
 	public Spa() {
 		description = "Luxury Spa";
 	}
+	
 	public double cost() {
 		return COST;
 	}
