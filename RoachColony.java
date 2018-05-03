e3abfaf9735d8096ae9e67f3f657041d08ec02e3
NullPointer0000 <38956871+NullPointer0000@users.noreply.github.com>
2018-05-03 10:04:40 -0700

    Update RoachColony.java

--- RoachColony.java
+++ RoachColony.java
@@ -7,6 +7,14 @@ package designpatternsproject;
  *
  * @author ericpalma
  */
-public class RoachColony {
-    
+public class RoachColony implements Observer{
+    /**
+	 * This method is called when the state of the subject
+	 * changes. 
+	 * @param newState - the new state of the subject
+	 */
+	public void update(boolean roomAvailable){
+        if(roomAvailable)
+            System.out.println("Waitlisted roach colony checking in.");
+    }
 }
