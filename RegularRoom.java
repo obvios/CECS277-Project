1c7a2fa171092d51fea0129db8db2f2310e377f3
Juan Carlos Pasillas <37219813+JCPasillas@users.noreply.github.com>
2018-05-03 09:46:10 -0700

    added rooms

---
+++ RegularRoom.java
@@ -0,0 +1,13 @@
+
+public class RegularRoom extends Room{
+	public static final double COST = 50.00;
+    public RegularRoom() {
+        description = "Regular Room";
+    }
+    public RegularRoom(String desc) {
+        description = desc;
+    }
+    public double cost() {
+        return COST;
+    }
+}
