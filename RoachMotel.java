9f4d53d0fd52739090e6b734d65936e2aeceaf39
NullPointer0000 <38956871+NullPointer0000@users.noreply.github.com>
2018-05-03 10:02:19 -0700

    Update RoachMotel.java

--- RoachMotel.java
+++ RoachMotel.java
@@ -4,16 +4,25 @@
  */
 package designpatternsproject;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  * @author ericpalma
  */
-public class RoachMotel {
+public class RoachMotel implements Subject {
     private static RoachMotel uniqueInstance;
-    private int initCapacityOfRooms = 50;           //number of rooms the motel has initially available.
-    private boolean noVacancySign = false;             //true when there are no vacant rooms
+    private static int initCapacityOfRooms = 50;           //number of rooms the motel has initially available.
+    private boolean noVacancySign;             //true when there are no vacant rooms
+    
+    private Queue<Observer> waitlist;
+    
     //private constructor
-    private RoachMotel(){};
+    private RoachMotel(){
+        waitlist = new LinkedList<Observer>();
+        noVacancySign = false;
+    };
     
     //returns this one and only instance of RoachMotel
     public synchronized static RoachMotel getInstance(){
@@ -36,5 +45,24 @@ public class RoachMotel {
             initCapacityOfRooms++;
             noVacancySign = false;
         }  
+    }
+    
+    /* allows an observer to register with the subject */
+	public void addObserver( Observer o ){
+        waitlist.add(o);
+        System.out.println("New roach colony waitlisted with position of " + waitlist.size() + ".");
+    }
+
+	/* removes an observer */	
+	public void removeObserver( Observer o ){
+        waitlist.poll();
+        System.out.println("Roach colony leaving waitlist and entering hotel.");
+        
+    }
+
+	/* notifes all registered observers when its state changes */	
+	public void notifyObservers(){
+        for (Observer roachColony : observers)
+			roachColony.update(noVacancySign);
     }
 }
