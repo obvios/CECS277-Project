ef94895ef06c82a04348cc5721002b85d9c05b6d
Eric Palma <palmae95@gmail.com>
2018-05-04 21:29:10 -0700

    added throwParty() method

--- Design Pattern Project/src/RoachColony.java
+++ Design Pattern Project/src/RoachColony.java
@@ -16,5 +16,10 @@ public class RoachColony implements Observer{
 	public void update(boolean vacancySign){
         if(!vacancySign)                                //if vacancy sign is turn off (false)                                            
             System.out.println("Waitlisted roach colony checking in.");
-    }
+        }
+        
+        //when roaches throw party, the multiply by their growth factor
+        public void throwParty(){
+            this.initialPopulation +=  (initialPopulation * growthRate);               //increase popluation by growthRate
+        }
 }
