8b17c980954f944c2cf8fd9be17be808c5649002
Eric Palma <palmae95@gmail.com>
2018-05-04 21:19:20 -0700

    Adjusted update()
    When roaches get a notification with the noVacancySign, they display message only if the sign is off (false)

--- Design Pattern Project/src/RoachColony.java
+++ Design Pattern Project/src/RoachColony.java
@@ -2,18 +2,15 @@
  * Class Name: RoachColony
  */
 
-/**
- *
- * @author ericpalma
- */
+
 public class RoachColony implements Observer{
     /**
 	 * This method is called when the state of the subject
 	 * changes. 
 	 * @param newState - the new state of the subject
 	 */
-	public void update(boolean roomAvailable){
-        if(roomAvailable)
+	public void update(boolean vacancySign){
+        if(!vacancySign)                                //if vacancy sign is turn off (false)                                            
             System.out.println("Waitlisted roach colony checking in.");
     }
 }
