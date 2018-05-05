d71b52cbb9ea52b6b2fa74beb881b86070880d65
Eric Palma <palmae95@gmail.com>
2018-05-04 20:39:43 -0700

    Adjusted constructor to get the Room type it is decorating

--- Design Pattern Project/src/FoodBar.java
+++ Design Pattern Project/src/FoodBar.java
@@ -1,10 +1,16 @@
 public class FoodBar extends Amnemities{
+        protected Room roomType;
 	public static final double COST = 10.00;
 	
-	public FoodBar() {
-		description = "Delicious Food Bar";
+	public FoodBar(Room theRoom) {
+		this.roomType = theRoom;
 	}
 	
+        @Override
+        public String getDescription() {
+            return roomType.getDescription() + " with a Food Bar";
+        }
+        
 	public double cost() {
 		return COST;
 	}
