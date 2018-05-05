7ec84d2f557172c718bf40310278edaa101bf412
Eric Palma <palmae95@gmail.com>
2018-05-04 21:25:43 -0700

    added constructor

--- Design Pattern Project/src/RoachColony.java
+++ Design Pattern Project/src/RoachColony.java
@@ -4,11 +4,15 @@
 
 
 public class RoachColony implements Observer{
-    /**
-	 * This method is called when the state of the subject
-	 * changes. 
-	 * @param newState - the new state of the subject
-	 */
+        private double growthRate;
+        private int initialPopulation;
+        
+        //constructor
+        public RoachColony(double growth, int population){
+            this.growthRate = growth;
+            this.initialPopulation = population;
+        }
+    
 	public void update(boolean vacancySign){
         if(!vacancySign)                                //if vacancy sign is turn off (false)                                            
             System.out.println("Waitlisted roach colony checking in.");
