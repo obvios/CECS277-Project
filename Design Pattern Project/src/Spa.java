9fab0cb36f99c5113281d7c58754deaf14834af0
Eric Palma <palmae95@gmail.com>
2018-05-04 20:43:46 -0700

    Adjusted constructor

--- Design Pattern Project/src/Spa.java
+++ Design Pattern Project/src/Spa.java
@@ -1,9 +1,15 @@
 public class Spa extends Amnemities{
+        protected Room roomType;
 	public static final double COST = 20.00;
 	
-	public Spa() {
-		description = "Luxury Spa";
+	public Spa(Room theRoom) {
+		this.roomType = theRoom;
 	}
+        
+        @Override
+        public String getDescription(){
+            return roomType.getDescription() + " with a spa";
+        }
 	
 	public double cost() {
 		return COST;
