299ec5825231d3757b33ee5db92cdcbe1e12e914
Eric Palma <palmae95@gmail.com>
2018-05-04 20:24:18 -0700

    Removed extra Regular room method

--- Design Pattern Project/src/DeluxeRoom.java
+++ Design Pattern Project/src/DeluxeRoom.java
@@ -1,13 +1,13 @@
-package designpatternsproject;
+/*
+This class creates a Deluxe Room
+*/
 
 public class DeluxeRoom extends Room{
 	public static final double COST = 75.00;
     public DeluxeRoom() {
         description = "Deluxe Room";
     }
-    public DeluxeRoom(String desc) {
-        description = desc;
-    }
+   
     public double cost() {
         return COST;
     }
