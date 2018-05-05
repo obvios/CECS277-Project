13466ea3e834bc75b2422022b344b8391f9d5f44
Eric Palma <palmae95@gmail.com>
2018-05-04 20:24:36 -0700

    Removed extra Regular Room method

--- Design Pattern Project/src/RegularRoom.java
+++ Design Pattern Project/src/RegularRoom.java
@@ -1,13 +1,13 @@
-package designpatternsproject;
+/*
+This class creates a Regular Room
+*/
 
 public class RegularRoom extends Room{
 	public static final double COST = 50.00;
     public RegularRoom() {
         description = "Regular Room";
     }
-    public RegularRoom(String desc) {
-        description = desc;
-    }
+   
     public double cost() {
         return COST;
     }
