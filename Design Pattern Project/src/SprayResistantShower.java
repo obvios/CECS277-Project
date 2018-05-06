422cb92c7b8afd7d89ffac1ff9b5bafc68e218d0
Eric Palma <palmae95@gmail.com>
2018-05-05 20:55:49 -0700

    Completed SprayResistantShower Amnenitie

--- Design Pattern Project/src/SprayResistantShower.java
+++ Design Pattern Project/src/SprayResistantShower.java
@@ -1,1 +1,20 @@
-
+/*
+This is an amnenitie that decorates a hotel room.
+*/
+public class SprayResistantShower extends HotelDecorator{
+    protected Room roomType;
+    public static final double COST = 25.00;
+    
+    public SprayResistantShower(Room theRoom){
+        this.roomType = theRoom;
+    }
+    
+    @Override
+    public String getDescription(){
+        return roomType.getDescription() + " with a Spray Resistant Shower";
+    }
+    
+    public double cost(){
+        return COST;
+    }
+}
