7e9f5fa08d53a229ef80499756af244307384da5
Eric Palma <palmae95@gmail.com>
2018-05-05 21:14:45 -0700

    added colony name and message colony displays when notified of open room

--- Design Pattern Project/src/RoachColony.java
+++ Design Pattern Project/src/RoachColony.java
@@ -6,16 +6,18 @@
 public class RoachColony implements Observer{
         private double growthRate;
         private int initialPopulation;
+        private String colonyName;
         
         //constructor
-        public RoachColony(double growth, int population){
+        public RoachColony(String name, double growth, int population){
+            this.colonyName = name;
             this.growthRate = growth;
             this.initialPopulation = population;
         }
     
 	public void update(boolean vacancySign){
-        if(!vacancySign)                                //if vacancy sign is turn off (false)                                            
-            System.out.println("Waitlisted roach colony checking in.");
+        if(!vacancySign)                                //if vacancy sign is turned off (false)                                            
+            System.out.println(colonyName + " received message");
         }
         
         //when roaches throw party, the multiply by their growth factor
