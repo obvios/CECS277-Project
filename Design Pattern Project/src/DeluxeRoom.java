5c5f8d9886b630e2869f9314fa9fb7d52aa30de7
JCPasillas <carlospasillas562@gmail.com>
2018-05-06 16:15:44 -0700

    check

--- Design Pattern Project/src/DeluxeRoom.java
+++ Design Pattern Project/src/DeluxeRoom.java
@@ -3,7 +3,8 @@ This class creates a Deluxe Room
 */
 
 public class DeluxeRoom extends Room{
-	public static final double COST = 75.00;
+    public static final double COST = 75.00;
+    
     public DeluxeRoom() {
         description = "Deluxe Room";
     }
--- Design Pattern Project/src/Room.java
+++ Design Pattern Project/src/Room.java
@@ -1,3 +1,4 @@
+
 public abstract class Room {
 	protected String description;
 
