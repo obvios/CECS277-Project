21cefd75a07773962f6507fc126fe9d2ebe36b22
Eric Palma <palmae95@gmail.com>
2018-05-07 20:31:48 -0700

    updates to room

--- Design Pattern Project/src/DeluxeRoom.java
+++ Design Pattern Project/src/DeluxeRoom.java
@@ -4,6 +4,7 @@ This class creates a Deluxe Room
 
 public class DeluxeRoom extends Room{
     public static final double COST = 75.00;
+    private RoachColony theColony;
     
     public DeluxeRoom() {
         description = "Deluxe Room";
@@ -11,6 +12,16 @@ public class DeluxeRoom extends Room{
    
     public double cost() {
         return COST;
+    }
+    
+    //adds a colony to the room
+    public void addColony(RoachColony colony){
+        this.theColony = colony;
+    }
+    
+    //returns the colony
+    public RoachColony getColony(){
+        return theColony;
     }
     
     @Override
--- Design Pattern Project/src/RegularRoom.java
+++ Design Pattern Project/src/RegularRoom.java
@@ -4,6 +4,7 @@ This class creates a Regular Room
 
 public class RegularRoom extends Room{
 	public static final double COST = 50.00;
+        private RoachColony theColony;
     public RegularRoom() {
         description = "Regular Room";
     }
@@ -12,4 +13,13 @@ public class RegularRoom extends Room{
         return COST;
     }
     
+    //adds a colony to the room
+    public void addColony(RoachColony colony){
+        this.theColony = colony;
+    }
+    
+    //returns the colony
+    public RoachColony getColony(){
+        return theColony;
+    }
 }
