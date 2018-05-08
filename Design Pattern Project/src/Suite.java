9c3e6bce90e028b8684204a7aeb0c426359b6b39
Shield-bro <justincdo@gmail.com>
2018-05-07 21:27:03 -0700

    bam

--- Design Pattern Project/src/Suite.java
+++ Design Pattern Project/src/Suite.java
@@ -1,28 +1,30 @@
 /*
 This class creates a Suite room
-*/
+ */
 
-public class Suite extends Room{
-	public static final double COST = 100.00;
-        private RoachColony theColony;
+public class Suite extends Room {
+
+    public static final double COST = 100.00;
+    private RoachColony theColony;
+
     public Suite() {
         description = "Suite";
     }
-    
+
     public double cost() {
         return COST;
     }
-    
-    public void addColony(RoachColony theColony){
+
+    public void addColony(RoachColony theColony) {
         this.theColony = theColony;
     }
-    
-    public RoachColony getColony(){
+
+    public RoachColony getColony() {
         return theColony;
     }
-    
+
     @Override
-        public String toString(){
-            return "Suite";
-        }
+    public String toString() {
+        return "Suite";
+    }
 }
