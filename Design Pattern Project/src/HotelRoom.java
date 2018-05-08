b24ebcf8bb4599aef9ca102b7d3d275bbfb63120
Shield-bro <justincdo@gmail.com>
2018-05-07 21:24:13 -0700

    CHANGE ALL THE FILES

--- Design Pattern Project/src/HotelRoom.java
+++ Design Pattern Project/src/HotelRoom.java
@@ -1,27 +1,25 @@
 /*
 This class is the hotel room factory.
 It is in charge of creating the different kinds of rooms
-*/
-package designpatternsproject;
+ */
 
 public class HotelRoom extends HotelRoomFactory {
-	
-	//returns a room
-	public Room getRoom(String type){
-            if(type.equals("Deluxe")){
-                return new DeluxeRoom();
-            }
-            else if (type.equals("Suite")){
-                return new Suite();
-            }
-            else if (type.equals("Regular")){
-                return new RegularRoom();
-            }
-            else return null;
+
+    //returns a room
+    public Room getRoom(String type) {
+        if (type.equals("Deluxe")) {
+            return new DeluxeRoom();
+        } else if (type.equals("Suite")) {
+            return new Suite();
+        } else if (type.equals("Regular")) {
+            return new RegularRoom();
+        } else {
+            return null;
         }
-        
+    }
+
     @Override
-        public String toString(){
-            return "Hotel Room Factory";
-        }
+    public String toString() {
+        return "Hotel Room Factory";
+    }
 }
--- Design Pattern Project/src/RoachColony.java
+++ Design Pattern Project/src/RoachColony.java
@@ -2,40 +2,38 @@
  * Class Name: RoachColony
  */
 
+public class RoachColony implements Observer {
 
-public class RoachColony implements Observer{
-        private double growthRate;
-        private int initialPopulation;
-        private String colonyName;
-        
-        //constructor
-        public RoachColony(String name, double growth, int population){
-            this.colonyName = name;
-            this.growthRate = growth;
-            this.initialPopulation = population;
-        }
-    
-	public void update(boolean vacancySign){
-        if(!vacancySign)                                //if vacancy sign is turned off (false)                                            
+    private double growthRate;
+    private int initialPopulation;
+    private String colonyName;
+
+    //constructor
+    public RoachColony(String name, double growth, int population) {
+        this.colonyName = name;
+        this.growthRate = growth;
+        this.initialPopulation = population;
+    }
+
+    public void update(boolean vacancySign) {
+        if (!vacancySign) //if vacancy sign is turned off (false)                                            
+        {
             System.out.println(colonyName + " received message");
         }
-        
-        //when roaches throw party, the multiply by their growth factor
-        public void throwParty(){
-            this.initialPopulation +=  (initialPopulation * growthRate);               //increase popluation by growthRate
-        }
-        
-        //this method is called when the roach colony is sprayed with insecticide. Reduces colony by percent
-        public void reduceColonyPopulation(double percent){
-            this.initialPopulation -= (initialPopulation * percent);
-        }
-        
-        public void getPopulation(){
-            return initialPopulation;
-        }
-        
-        public String toString(){
-            String i = "Colony Name: " + colonyName + " Poplulation " + initialPopulation;
-            return i;
-        }
+    }
+
+    //when roaches throw party, the multiply by their growth factor
+    public void throwParty() {
+        this.initialPopulation += (initialPopulation * growthRate);               //increase popluation by growthRate
+    }
+
+    //this method is called when the roach colony is sprayed with insecticide. Reduces colony by percent
+    public void reduceColonyPopulation(double percent) {
+        this.initialPopulation -= (initialPopulation * percent);
+    }
+
+    public String toString() {
+        String i = "Colony Name: " + colonyName + " Poplulation " + initialPopulation;
+        return i;
+    }
 }
