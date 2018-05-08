1a6270b3a5ede079a399fa37c2539b60b9b27805
Shield-bro <justincdo@gmail.com>
2018-05-07 20:31:34 -0700

    dedited suit

--- Design Pattern Project/src/Suite.java
+++ Design Pattern Project/src/Suite.java
@@ -4,12 +4,21 @@ This class creates a Suite room
 
 public class Suite extends Room{
 	public static final double COST = 100.00;
+        private RoachColony theColony;
     public Suite() {
         description = "Suite";
     }
     
     public double cost() {
         return COST;
+    }
+    
+    public void addColony(RoachColony theColony){
+        this.theColony = theColony;
+    }
+    
+    public RoachColony getColony(){
+        return theColony;
     }
     
     @Override
